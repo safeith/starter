fcb16d9781849b1feb6d429c8701a60f07de3137
Hojjat Ali Mohammadi <safeith@gmail.com>
2024-03-16 19:59:49 +0100

    Adding shortcuts for enabling/disabling formatter

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -13,7 +13,9 @@ end, { desc = "File Format with conform" })
 map("i", "jk", "<ESC>", { desc = "Escape insert mode" })
 
 -- Tmux navigation
-map("n", "<C-h>", "<cmd> TmuxNavigateLeft<CR>", {desc= "Navigate Left"})
-map("n", "<C-j>", "<cmd> TmuxNavigateDown<CR>", {desc= "Navigate Down"})
-map("n", "<C-k>", "<cmd> TmuxNavigateUp<CR>", {desc= "Navigate Up"})
-map("n", "<C-l>", "<cmd> TmuxNavigateRight<CR>", {desc= "Navigate Right"})
+map("n", "<C-h>", "<cmd> TmuxNavigateLeft<CR>", { desc = "Navigate Left" })
+map("n", "<C-j>", "<cmd> TmuxNavigateDown<CR>", { desc = "Navigate Down" })
+map("n", "<C-k>", "<cmd> TmuxNavigateUp<CR>", { desc = "Navigate Up" })
+map("n", "<C-l>", "<cmd> TmuxNavigateRight<CR>", { desc = "Navigate Right" })
+map("n", "<leader>fe", "<cmd> FormatEnable<CR>", { desc = "Enable formatter" })
+map("n", "<leader>fd", "<cmd> FormatDisable<CR>", { desc = "Disable formatter" })
